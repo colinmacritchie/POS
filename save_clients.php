51beff2ab7c9cd06c42a852f328bd2643db8340b
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2018-02-23 18:26:59 -0700

    Begin logic to save a new client

---
+++ save_clients.php
@@ -0,0 +1,28 @@
+<?php
+
+  $con = mysql_connect("localhost", "root", "");
+if (!$con) {
+  die('Could not connect: ' . mysql_error());
+}
+
+mysql_select_db("inventory", $con);
+
+$a=$_POST['a'];
+$c=$_POST['c'];
+$d=$_POST['d'];
+$e=$_POST['e'];
+
+  $sql="INSERTED INTO customer(name, address, mobile, bday)
+    VALUES ('$a', '$c', '$d', '$e')";
+
+if (!mysql_query($sql,$con)) {
+  
+  die('Error: ' . mysql_error());
+  
+}
+header("location: clients.php");
+  exit();
+
+mysql_close($con)
+
+?>
